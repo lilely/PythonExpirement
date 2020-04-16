0af6ef2ef900c3b5a6ed3e062fe11e396b047877
jinxing <jinxing@edaijia-inc.cn>
2020-04-16 18:44:21 +0800

    算法联系

---
+++ Algorithm/SearchInTrie.py
@@ -0,0 +1,58 @@
+class TrieNode:
+    def __init__(self, children, isWord):
+        self.children = children
+        self.isWord = isWord
+
+class Solution:
+    def __init__(self):
+        self.trie = None
+
+    def build(self, words):
+        self.trie = TrieNode({},False)
+        for word in words:
+            current = self.trie
+            for char in word:
+                if not char in current.children:
+                    current.children[char] = TrieNode({},False)
+                current = current.children[char]
+            current.isWord = True
+        self.dumpTrie(self.trie)
+
+    def dumpTrie(self, trieNode):
+        for char in trieNode.children:
+            print(char+"->")
+        for char in trieNode.children:
+            self.dumpTrie(trieNode.children[char])
+
+    def findTheWord(self, current ,prefix):
+        result = []
+        if current.isWord == True:
+            print(prefix)
+            result.append(prefix) 
+        for char in current.children:
+            if current.children[char].isWord == True:
+                # print current.children[char]
+                print(prefix+char)
+                result.append(prefix+char)
+            else:
+                result.extend(self.findTheWord(current.children[char], prefix+char))
+        return result
+
+    def autoComplete(self, prefix):
+        current = self.trie
+        result = []
+        for char in prefix:
+            if not char in current.children:
+                return []
+            current = current.children[char]
+        ret = self.findTheWord(current, prefix)
+        if ret == None:
+            print("ret is None")
+        result.extend(ret)
+        return result
+        
+
+solution = Solution()
+solution.build(["op","ot","oq","opx","oww","op123"])
+result = solution.autoComplete("op")
+print(result)
